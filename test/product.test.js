32fc081b197681c80da16583a3352407ebc6d147
CHENG-TING CHEN <abfa762466@gmail.com>
2024-05-19 14:45:37 +0800

    add new test

--- test/product.test.js
+++ test/product.test.js
@@ -47,36 +47,39 @@ describe("API /api/products TEST START...", () => {
         expect(res.body.data.products.length).toBe(productData.length);
     });
 
-    it("GET /api/products/:id | should return one product with corresponded product ", async () => {
-        await request(app).post("/api/products").send(productData);
-        let res = await request(app).get("/api/products");
-        console.log(res.body.data.products);
-        const randomNumber = productData.length;
-        const id = res.body.data.products[randomNumber - 1]._id;
-        console.log("id", id);
+    // it("GET /api/products/:id | should return one product with corresponded product ", async () => {
+    //     let res = await request(app).post("/api/products").send(productData);
+    //     console.log(res.body.data.products);
+    //     res = await request(app).get("/api/products");
+    //     console.log(res.body.data.products);
+    //     const randomNumber = productData.length;
+    //     console.log(res.body.data.products[randomNumber]);
+    // const id = res.body.data.products[randomNumber]._id;
+    // console.log("id", id);
 
-        res = await request(app).get(`/api/products/${id}`);
-        console.log(res.body);
+    // res = await request(app).get(`/api/products/${id}`);
+    // console.log(res.body);
 
-        // expect(res.body.status).toBe("success");
-        // expect(res.statusCode).toBe(200);
-        // expect(res.body.data.product._id).toBe(id);
-    });
-
-    // it("POST /api/products | should add a new product to database", async () => {
-    //     const newProduct = {
-    //         name: "TEST PRODUCT",
-    //         description: "THIS IS THE ONE AND ONLY TEST PRODUCT",
-    //         price: 100000,
-    //     };
-    //     let res = await request(app).post("/api/products").send(newProduct);
-    //     expect(res.statusCode).toBe(201);
-    //     expect(res.body.data.product.name).toBe("TEST PRODUCT");
-
-    //     // create same product should return statusCode 409
-    //     res = await request(app).post("/api/products").send(newProduct);
-    //     expect(res.statusCode).toBe(409);
+    // expect(res.body.status).toBe("success");
+    // expect(res.statusCode).toBe(200);
+    // expect(res.body.data.product._id).toBe(id);
     // });
+
+    it("POST /api/products | should add a new product to database", async () => {
+        const newProduct = {
+            name: "TEST PRODUCT",
+            description: "THIS IS THE ONE AND ONLY TEST PRODUCT",
+            price: 100000,
+        };
+        let res = await request(app).post("/api/products").send(newProduct);
+        console.log(res.body.data);
+        expect(res.statusCode).toBe(201);
+        expect(res.body.data.product.name).toBe("TEST PRODUCT");
+
+        // create same product should return statusCode 409
+        res = await request(app).post("/api/products").send(newProduct);
+        expect(res.statusCode).toBe(409);
+    });
 
     // it("PATCH /api/products/:id | update a specified product by id", async () => {
     //     // create a new product
